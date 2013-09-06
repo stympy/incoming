b82d7513f32dfce9d101ae29fd28165cd4e6210a
Benjamin Curtis <benjamin.curtis@gmail.com>
2013-09-06 15:18:30 -0700

    Avoiding a problem with the json_pure gem

--- lib/incoming.rb
+++ lib/incoming.rb
@@ -7,7 +7,6 @@ module Incoming
   module Strategies
     autoload :SendGrid, 'incoming/strategies/sendgrid'
     autoload :Mailgun, 'incoming/strategies/mailgun'
-    autoload :Postmark, 'incoming/strategies/postmark'
     autoload :CloudMailin, 'incoming/strategies/cloudmailin'
     autoload :HTTPPost, 'incoming/strategies/http_post'
   end
